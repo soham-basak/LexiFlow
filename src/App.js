e960638daeba866f2c4251662e4f379abe877149
dexter <sohambasak42@gmail.com>
2023-06-29 20:55:45 +0530

    feat(core): add create blog page to UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Single from './pages/Single/Single' 
 import TopBar from "./components/topbar/TopBar"
 import Home from "./pages/Home/Home"
+import Write from './pages/Write/Write';
 
 function App() {
   return (
     <>
       <TopBar/>
-      <Single/>
+      <Write/>
     </>
   );
 }
---
+++ src/pages/Write/Write.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import image from './writeImage.jpg'
+import './Write.css'
+
+export default function Write() {
+  return (
+    <div className='write'>
+        <img className='writeImage' 
+        src={image} alt="" />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+            <label htmlFor="fileInput">
+            <i className="writeIcon fa-solid fa-plus"></i>
+            </label>
+            <input type="file" id="fileInput" 
+             style={{display: 'none'}}
+            />
+            <input type="text" placeholder='Title' className='writeInput' autoFocus={true}/>
+        </div>
+        <div className="writeFormGroup">
+            <textarea placeholder="Tell your story..." type="text" className="writeInput writeText"></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </div>
+  )
+}
