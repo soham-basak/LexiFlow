3adab5749c033d473455df4051d9731034c3d58b
dexter <sohambasak42@gmail.com>
2023-06-30 15:52:32 +0530

    initial commit: add models and db connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(console.log('connect')).catch((err) => console.log(err));
 
 app.listen("5000", ()=>{
     console.log("listening on");
