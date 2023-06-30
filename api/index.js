dbbaac3dea97cd833ce1607fdff006879364a9da
dexter <sohambasak42@gmail.com>
2023-06-30 18:52:05 +0530

    feat: add register route

--- api/index.js
+++ api/index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(console.log('connect')).catch((err) => console.log(err));
 
+app.use("/api/auth", authRoute);
+
 app.listen("5000", ()=>{
     console.log("listening on");
-})
+});
